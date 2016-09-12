01121b707f717ba2c0300ce2e7dce1cb163fd1f0
SajuukThanatoskhar <gareth_willia64@hotmail.com>
2016-09-13 07:22:30 +1000

    Adding flow control file

---
+++ Switch_script.m
@@ -0,0 +1,19 @@
+%switch script
+
+A = 10;
+
+switch(A)
+    case 10
+        fprintf('A is 10\n')
+        
+    case 8 
+        fprintf('A is 8\n')
+        
+    case 7 
+        fprintf('A is 7\n')
+        
+    case 5
+        fprintf('A is 5\n')
+    otherwise 
+        fprintf('A isnt anything\n')
+end
---
+++ if_script.m
@@ -0,0 +1,19 @@
+%If Statement Script
+
+A = 5
+B = 2
+C = 10
+
+if A > 2
+    fprintf('A is bigger than 2')
+end
+
+if A != 5
+    fprintf('A is no equal to 5')
+elseif A*B == C
+    fprintf('A*B is equal to C')
+else
+    fprintf('I got nothin''')
+end
+    
+    
